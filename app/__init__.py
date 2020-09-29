bc43d39141b1f9268b32c6e8436cd626977d4b01
Juan <sjdonado@uninorte.edu.co>
2020-09-29 18:03:43 -0500

    feat: seconddawork

--- app/__init__.py
+++ app/__init__.py
@@ -9,3 +9,7 @@ create_open_estimation_dash_app(server)
 @server.route('/')
 def index():
 	return render_template('index.html', title='Applied Math and Computer Science Lab')
+
+@server.route('/seconddawork')
+def index():
+	return render_template('seconddawork.html', title='Second International Workshop on Data Assimilation for Decision Making, Colombia ')
